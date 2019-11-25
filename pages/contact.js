faa8edff78e4142dd23914318fcf2cbe4098efd7
Jacob Hoffman <jaketh19@gmail.com>
2019-11-25 17:29:29 -0500

    added info to the contact page

--- pages/contact.js
+++ pages/contact.js
@@ -6,9 +6,35 @@ export default function Contact() {
         <Layout>
             <div id="contact-container">
                 <h1 id="title">Contact</h1>
+
+                <div id ="contact-info">
+                    <ul id ="field">
+                        <li> Name.................................Jacob Hoffman </li>
+                        <li> E-mail.....................jthoffman@liberty.edu </li>
+                        <li> Phone...................................610-413-7348 </li>
+                    </ul>
+                </div>
             </div>
             
             <style jsx>{`
+            #field li{
+                list-style: none;
+                margin: 0;
+                padding-top: 14px;
+            }
+
+            #contact-info {
+                width: 18%;
+                margin-top: 2%;
+                margin-left: 41%;
+
+                display: inline-flex;
+                color: white;
+                font-family: 'Roboto', open-sans;
+                font-size: 1em;
+                justify-content: space-between;
+            }
+
             #contact-container {
                 position: relative;
                 height: 100%;
